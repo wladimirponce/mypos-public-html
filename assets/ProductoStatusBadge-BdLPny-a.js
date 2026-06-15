d50c64f7cbb303acc0eae4938d9d354b0e2f8a41
Wladimir Ponce <wladimirponce@gmail.com>
2026-06-15 16:01:35 -0400

    Deploy MyPOS 2026-06-15 16:01:35

--- assets/ProductoStatusBadge-BdLPny-a.js
+++
@@ -1,1 +0,0 @@
-import{au as o,B as n}from"./index-BbxvQ2XV.js";function s({activo:e}){const t=e===void 0?!0:!!Number(e);return o.jsx(n,{tone:t?"success":"neutral",children:t?"Activo":"Inactivo"})}export{s as P};
--- assets/coerce-C86BBjsw.js
+++
@@ -1,1 +0,0 @@
-import{a6 as e,a5 as o}from"./index-BbxvQ2XV.js";function m(r){return e(o,r)}export{m as n};
