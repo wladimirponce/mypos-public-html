ee6471cc4fa816f7b58e33f39847b3e03ab6b5d8
Wladimir Ponce <wladimirponce@gmail.com>
2026-06-27 19:41:40 -0400

    Deploy MyPOS 2026-06-27 19:41:39

---
+++ assets/coerce-pEDdRpUU.js
@@ -0,0 +1,1 @@
+import{ai as e,ah as o}from"./index-LqHhj_ao.js";function m(r){return e(o,r)}export{m as n};
---
+++ assets/useDashboardData-DRU9PpnY.js
@@ -0,0 +1,1 @@
+import{b1 as a}from"./index-LqHhj_ao.js";import{r}from"./reporteService-DW2kBGoZ.js";function s(e){return a({queryKey:["dashboard",e.empresa_id,e.sucursal_id,e.fecha_desde,e.fecha_hasta],queryFn:()=>r.getDashboard(e),enabled:e.empresa_id>0,staleTime:3e4})}export{s as u};
