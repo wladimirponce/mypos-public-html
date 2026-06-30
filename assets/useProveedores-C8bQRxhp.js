f0bcbe109717f0bc0e3db067d3695c46f12cf977
Wladimir Ponce <wladimirponce@gmail.com>
2026-06-30 15:14:00 -0400

    Deploy MyPOS 2026-06-30 15:14:00

---
+++ assets/useProveedores-C8bQRxhp.js
@@ -0,0 +1,1 @@
+import{aG as a,aN as s}from"./index-NzWm7Sby.js";import{n as i,c as t}from"./compraService-CyEP7bc7.js";function d(r=""){const e=a(o=>o.empresaActiva);return s({queryKey:["proveedores",e==null?void 0:e.empresa_id,r],queryFn:async()=>i(await t.providers((e==null?void 0:e.empresa_id)??0,r)),enabled:!!(e!=null&&e.empresa_id),staleTime:6e4})}export{d as u};
