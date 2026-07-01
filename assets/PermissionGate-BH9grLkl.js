f6c0b62ddb5ba81c44d84024584152b6f3d5bae3
Wladimir Ponce <wladimirponce@gmail.com>
2026-07-01 11:25:54 -0400

    Deploy MyPOS 2026-07-01 11:25:54

--- assets/PermissionGate-BH9grLkl.js
+++
@@ -1,1 +0,0 @@
-import{aG as o,ar as s}from"./index-DhU0-Jsp.js";function h({permission:n,anyOf:r,fallback:t=null,children:a}){const i=o(e=>e.hasPermission),m=o(e=>e.hasAnyPermission);return(n?i(n):r?m(r):!0)?s.jsx(s.Fragment,{children:a}):s.jsx(s.Fragment,{children:t})}export{h as P};
